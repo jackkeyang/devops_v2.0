c8d0a168135549feabdbd5622deb4074c8b3765e
USURETECH\yangke <jack_keyang@163.com>
2016-11-24 17:29:02 +0800

    add job table

--- reboot.sql
+++ reboot.sql
@@ -1,27 +1,24 @@
--- MySQL dump 10.13  Distrib 5.7.16, for Linux (x86_64)
---
--- Host: localhost    Database: reboot
--- ------------------------------------------------------
--- Server version	5.7.16-0ubuntu0.16.04.1
+/*
+Navicat MySQL Data Transfer
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+Source Server         : 93
+Source Server Version : 50629
+Source Host           : 172.16.0.93:3306
+Source Database       : flask
 
---
--- Table structure for table `idc`
---
+Target Server Type    : MYSQL
+Target Server Version : 50629
+File Encoding         : 65001
 
+Date: 2016-11-24 17:07:54
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for idc
+-- ----------------------------
 DROP TABLE IF EXISTS `idc`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `idc` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
@@ -29,25 +26,42 @@ CREATE TABLE `idc` (
   `address` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `idc`
---
+-- ----------------------------
+-- Records of idc
+-- ----------------------------
+INSERT INTO `idc` VALUES ('2', 'aliyun', '阿里云', '杭州1区');
 
-LOCK TABLES `idc` WRITE;
-/*!40000 ALTER TABLE `idc` DISABLE KEYS */;
-INSERT INTO `idc` VALUES (2,'aliyun','阿里云','杭州1区');
-/*!40000 ALTER TABLE `idc` ENABLE KEYS */;
-UNLOCK TABLES;
+-- ----------------------------
+-- Table structure for job
+-- ----------------------------
+DROP TABLE IF EXISTS `job`;
+CREATE TABLE `job` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `app_name` char(50) DEFAULT NULL COMMENT '应用系统名称',
+  `app_purpose` char(50) DEFAULT NULL COMMENT '系统用途说明',
+  `language` char(10) DEFAULT NULL COMMENT '开发语言',
+  `soft_version` varchar(100) DEFAULT NULL COMMENT '所需tomcat/jdk版本',
+  `sys_resource` char(100) DEFAULT NULL COMMENT '对系统资源需求',
+  `db_info` char(100) DEFAULT NULL COMMENT '是否使用数据库，容量需求',
+  `other_relation` char(100) DEFAULT NULL COMMENT '其他关联系统',
+  `sys_monitor` char(100) DEFAULT NULL COMMENT '监控方式(端口、api)',
+  `app_user` char(20) DEFAULT NULL COMMENT '系统使用人员',
+  `dever` char(20) DEFAULT NULL COMMENT '项目负责人',
+  `test_host` char(20) DEFAULT NULL COMMENT '测试服务器',
+  `pre_host` char(20) DEFAULT NULL COMMENT '预发布服务器',
+  `real_host` char(20) DEFAULT NULL COMMENT '生产服务器',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `user`
---
+-- ----------------------------
+-- Records of job
+-- ----------------------------
 
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
 DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
@@ -59,25 +73,18 @@ CREATE TABLE `user` (
   `status` int(11) NOT NULL COMMENT '0：激活，1：锁定',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'admin','超级管理员','a75b19b4ee4815f24c490ac57cc422d3','123@qq.com','111','admin',0),(4,'test','test','ad765b4729f3cb933fed381d408ddfab','123','123','user',0),(5,'a','a','944b6d5c64234b25e00be9618df620f4','121','112','user',0),(6,'pc','pc','e0606ed280e90376fdabc87c317c7099','12','12','user',0),(8,'bb','bb','944b6d5c64234b25e00be9618df620f4','1','1','user',0),(9,'cc','vc','9d7f008b447a5dbd042bd479dd578d32','1','1','user',0),(10,'dd','dd','e0606ed280e90376fdabc87c317c7099','12','12','user',0),(11,'ee','ee','944b6d5c64234b25e00be9618df620f4','1','1','user',0),(12,'ff','ff','944b6d5c64234b25e00be9618df620f4','1','1','user',0),(14,'fff','fff','d06008f36f96ffb168180d5cd5ef3bd3','1','1','admin',0);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-11-13 16:07:36
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'admin', '超级管理员', 'a75b19b4ee4815f24c490ac57cc422d3', '123@qq.com', '111', 'admin', '0');
+INSERT INTO `user` VALUES ('4', 'test', 'test', 'ad765b4729f3cb933fed381d408ddfab', '123', '123', 'user', '0');
+INSERT INTO `user` VALUES ('5', 'a', 'a', '944b6d5c64234b25e00be9618df620f4', '121', '112', 'user', '0');
+INSERT INTO `user` VALUES ('6', 'pc', 'pc', 'e0606ed280e90376fdabc87c317c7099', '12', '12', 'user', '0');
+INSERT INTO `user` VALUES ('8', 'bb', 'bb', '944b6d5c64234b25e00be9618df620f4', '1', '1', 'user', '0');
+INSERT INTO `user` VALUES ('9', 'cc', 'vc', '9d7f008b447a5dbd042bd479dd578d32', '1', '1', 'user', '0');
+INSERT INTO `user` VALUES ('10', 'dd', 'dd', 'e0606ed280e90376fdabc87c317c7099', '12', '12', 'user', '0');
+INSERT INTO `user` VALUES ('11', 'ee', 'ee', '944b6d5c64234b25e00be9618df620f4', '1', '1', 'user', '0');
+INSERT INTO `user` VALUES ('12', 'ff', 'ff', '944b6d5c64234b25e00be9618df620f4', '1', '1', 'user', '0');
+INSERT INTO `user` VALUES ('14', 'fff', 'fff', 'd06008f36f96ffb168180d5cd5ef3bd3', '1', '1', 'admin', '0');
+SET FOREIGN_KEY_CHECKS=1;
